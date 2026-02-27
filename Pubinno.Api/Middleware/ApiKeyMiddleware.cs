89f391beaafbe946053df2ca82ecd45ee099c9cc
Uğur Demir <ardahan1984@hotmail.com>
2026-02-27 13:22:39 +0300

    tyoken update

--- Pubinno.Api/Middleware/ApiKeyMiddleware.cs
+++ Pubinno.Api/Middleware/ApiKeyMiddleware.cs
@@ -3,17 +3,18 @@
 public class ApiKeyMiddleware
 {
     private readonly RequestDelegate _next;
-    private const string APIKEY = "demo-token";
-
-    public ApiKeyMiddleware(RequestDelegate next)
+    //private const string APIKEY = "demo-token";
+    private readonly string _apiKey;
+    public ApiKeyMiddleware(RequestDelegate next,IConfiguration _config)
     {
         _next = next;
+        _apiKey = _config.GetValue<string>("API_KEY") ?? throw new ArgumentNullException("APIKEY not found in configuration");
     }
 
     public async Task Invoke(HttpContext context)
     {
         if (!context.Request.Headers.TryGetValue("X-API-Key", out var key) ||
-            key != APIKEY)
+            key != _apiKey)
         {
             context.Response.StatusCode = 401;
             await context.Response.WriteAsync("Unauthorized");
